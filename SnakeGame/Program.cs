884af5db99fabf99b4d169b9080fbbaaa83b6075
mkytomies <moonakytomies@gmail.com>
2024-02-13 11:55:36 +0100

    Moved stuff to GUI-class and added score count

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -13,21 +13,12 @@ namespace Snake
                 Console.WindowWidth = 32;
             }
 
-            var rand = new Random ();
-
-            var score = 5;
-
-            // Initialize head and target position
-            var head = new Pixel (WindowWidth / 2, WindowHeight / 2, ConsoleColor.Red);
-            var target = new Pixel (rand.Next (1, WindowWidth - 2), rand.Next (1, WindowHeight - 2), ConsoleColor.Cyan);
-
-            // Initialize body of the snake
-            var body = new List<Pixel> ();
+            int score = 5;
 
             // Initialize movement direction
             var currentMovement = Direction.Right;
 
-            var gameover = false;
+            bool gameover = false;
 
             // Game loop
             while (true)
@@ -35,32 +26,36 @@ namespace Snake
                 Clear ();
 
                 // Check for collision with borders, if there is collision switch gameover variable to true
-                gameover |= head.XPos == WindowWidth - 1 || head.XPos == 0 || head.YPos == WindowHeight - 1 || head.YPos == 0;
+                gameover |= GUI.head.XPos == WindowWidth - 1 || GUI.head.XPos == 0 || GUI.head.YPos == WindowHeight - 1 || GUI.head.YPos == 0;
 
-                DrawBorder ();
+                GUI.DrawBorder ();
+
+                GetScore(gameover, score);
 
                 // Check for collision with target, if snake gets the target add 1 to score
-                if (target.XPos == head.XPos && target.YPos == head.YPos)
+                if (GUI.target.XPos == GUI.head.XPos && GUI.target.YPos == GUI.head.YPos)
                 {
                     score++;
                     // Add a new target 
-                    target = new Pixel (rand.Next (1, WindowWidth - 2), rand.Next (1, WindowHeight - 2), ConsoleColor.Cyan);
+                    GUI.target = new GUI.Pixel (GUI.rand.Next (1, WindowWidth - 2), GUI.rand.Next (1, WindowHeight - 2), ConsoleColor.Cyan);
+                    GetScore(gameover, score);
                 }
 
                 // Draw body for the snake
-                for (int i = 0; i < body.Count; i++)
+                for (int i = 0; i < GUI.body.Count; i++)
                 {
-                    DrawPixel (body[i]);
-                    gameover |= body[i].XPos == head.XPos && body[i].YPos == head.YPos;
+                    GUI.DrawPixel (GUI.body[i]);
+                    gameover |= GUI.body[i].XPos == GUI.head.XPos && GUI.body[i].YPos == GUI.head.YPos;
                 }
 
                 if (gameover)
                 {
+                    GetScore(gameover, score);
                     break;
                 }
 
-                DrawPixel (head);
-                DrawPixel (target);
+                GUI.DrawPixel (GUI.head);
+                GUI.DrawPixel (GUI.target);
 
                 // Control game speed
                 var stopWatch = Stopwatch.StartNew();
@@ -70,64 +65,44 @@ namespace Snake
                 }
 
                 // Add new head position to snake body
-                body.Add (new Pixel (head.XPos, head.YPos, ConsoleColor.Green));
+                GUI.body.Add (new GUI.Pixel (GUI.head.XPos, GUI.head.YPos, ConsoleColor.Green));
 
                 switch (currentMovement)
                 {
                     case Direction.Up:
-                        head.YPos--;
+                        GUI.head.YPos--;
                         break;
                     case Direction.Down:
-                        head.YPos++;
+                        GUI.head.YPos++;
                         break;
                     case Direction.Left:
-                        head.XPos--;
+                        GUI.head.XPos--;
                         break;
                     case Direction.Right:
-                        head.XPos++;
+                        GUI.head.XPos++;
                         break;
                 }
 
                 // Remove tail if snake exceeds its length
-                if (body.Count > score)
+                if (GUI.body.Count > score)
                 {
-                    body.RemoveAt (0);
+                    GUI.body.RemoveAt (0);
                 }
             }
-            
-            // Tell user that the game is over
-            SetCursorPosition (WindowWidth / 5, WindowHeight / 2);
-            WriteLine ($"Game over, Score: {score - 5}");
-            SetCursorPosition (WindowWidth / 5, WindowHeight / 2 + 1);
         }
 
-        static void DrawBorder ()
+        static void GetScore (bool gameover, int score) 
         {
-            for (int i = 0; i < WindowWidth; i++)
+            if (gameover) 
             {
-                SetCursorPosition (i, 0);
-                Write ("■");
-
-                SetCursorPosition (i, WindowHeight - 1);
-                Write ("■");
-            }
-
-            for (int i = 0; i < WindowHeight; i++)
-            {
-                SetCursorPosition (1, i);
-                Write ("■");
-
-                SetCursorPosition (WindowWidth - 2, i);
-                Write ("■");
-            }
-        }
-
-        static void DrawPixel (Pixel pixel)
-        {
-            SetCursorPosition (pixel.XPos, pixel.YPos);
-            ForegroundColor = pixel.ScreenColor;
-            Write ("■");
-            SetCursorPosition (0, 0); // Move cursor to prevent flickering
+                SetCursorPosition (WindowWidth / 5, WindowHeight / 2);
+                WriteLine ($"Game over, Score: {score - 5}");
+                SetCursorPosition (WindowWidth / 5, WindowHeight / 2 + 1);
+            } else 
+                {
+                    SetCursorPosition (WindowWidth / 5, WindowHeight - 3);
+                    WriteLine ($"Your score: {score - 5}");
+                }
         }
 
         static Direction GetNextMovement (Direction currentMovement)
@@ -152,19 +127,6 @@ namespace Snake
             return currentMovement;
         }
 
-        // Struct to represent a pixel (character) on the console
-        struct Pixel
-        {
-            public Pixel (int xPos, int yPos, ConsoleColor color)
-            {
-                XPos = xPos;
-                YPos = yPos;
-                ScreenColor = color;
-            }
-            public int XPos { get; set; }
-            public int YPos { get; set; }
-            public ConsoleColor ScreenColor { get; set; }
-        }
 
         enum Direction
         {
@@ -177,6 +139,56 @@ namespace Snake
 
     class GUI
     {
+        public static Random rand = new Random ();
 
+        // Initialize head and target position
+        public static Pixel head = new Pixel (WindowWidth / 2, WindowHeight / 2, ConsoleColor.Red);
+        public static Pixel target = new Pixel (rand.Next (1, WindowWidth - 2), rand.Next (1, WindowHeight - 2), ConsoleColor.Cyan);
+
+        // Initialize body of the snake
+        public static List<Pixel> body = new List<Pixel> ();
+
+        public static void DrawBorder ()
+        {
+            for (int i = 0; i < WindowWidth; i++)
+            {
+                SetCursorPosition (i, 0);
+                Write ("■");
+
+                SetCursorPosition (i, WindowHeight - 1);
+                Write ("■");
+            }
+
+            for (int i = 0; i < WindowHeight; i++)
+            {
+                SetCursorPosition (1, i);
+                Write ("■");
+
+                SetCursorPosition (WindowWidth - 2, i);
+                Write ("■");
+            }
+        }
+
+        public static void DrawPixel (Pixel pixel)
+        {
+            SetCursorPosition (pixel.XPos, pixel.YPos);
+            ForegroundColor = pixel.ScreenColor;
+            Write ("■");
+            SetCursorPosition (0, 0); // Move cursor to prevent flickering
+        }
+
+        // Struct to represent a pixel (character) on the console
+        public struct Pixel
+        {
+            public Pixel (int xPos, int yPos, ConsoleColor color)
+            {
+                XPos = xPos;
+                YPos = yPos;
+                ScreenColor = color;
+            }
+            public int XPos { get; set; }
+            public int YPos { get; set; }
+            public ConsoleColor ScreenColor { get; set; }
+        }
     }
 }
